c8d2be10d7359ac6d9ade8bebd3982c598699d82
hubert <hubertszostek@gmail.com>
2013-11-14 14:59:32 +0100

    ParsimLM implementation (not finished)

--- src/retrieval/ParsimLM.java
+++ src/retrieval/ParsimLM.java
@@ -17,23 +17,42 @@ public class ParsimLM implements IRetrievalModel{
 		
 		String [] query=queryObject.getQuery();
 		
-		HashMap <String, Double> rank = new HashMap <String, Double>();
+		HashMap <String, Double> PtC = new HashMap <String, Double>();//key: term
+        HashMap <String, HashMap<String,Double>> PtD = new HashMap <String, HashMap<String,Double>>();// key: document,
+        //value: term and probability value
 		double score = 0;
 		
-		int csum = 0; //occurrecies of all the terms in the collection
-		int [] qsum = new int[query.length];
+		double csum = 0; //occurrecies of all the terms in the collection
+		HashMap<String,Double> qsum = new HashMap<String, Double>();
 		
 		for(String doc: docList.keySet()){
-			int i = 0;
-			for(String term: query){
+            double sum=0;
+			for(String term: invIndex.keySet())
+            {
 				if(!invIndex.containsKey(term)) continue;
 				if(!invIndex.get(term).containsKey(doc)) continue;
 				int x = invIndex.get(term).get(doc);
 				csum += x;
-				qsum[i] += x;
-				i++;
+				sum += x;
 			}
+            qsum.put(doc,sum);
 		}
+        for(String doc: docList.keySet()){
+            double sum=0;
+            PtD.put(doc,new HashMap<String, Double>());
+            for(String term: invIndex.keySet())
+            {
+                if(!invIndex.containsKey(term)) continue;
+                if(!invIndex.get(term).containsKey(doc)) continue;
+                int tf = invIndex.get(term).get(doc);
+                double probabilityD=(double)tf/qsum.get(doc);
+                double probabilityC=(double)tf/csum;
+                PtC.put(term,probabilityC);
+                PtD.get(doc).put(term,probabilityD);
+            }
+            qsum.put(doc,sum);
+        }
+
 		
 		
 		//calculate the probabilities P(t|D) for each document
